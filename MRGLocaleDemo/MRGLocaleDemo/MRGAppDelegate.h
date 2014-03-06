12349081b610b48a25b810ff06b3388708b16b84
Vincent Roy Chevalier <vroychevalier@mirego.com>
2014-03-06 15:08:55 -0500

    Add demo project with pod && control panel

---
+++ MRGLocaleDemo/MRGLocaleDemo/MRGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MRGAppDelegate.h
+//  MRGLocaleDemo
+//
+//  Created by Vincent Roy Chevalier on 2014-03-06.
+//  Copyright (c) 2014 Mirego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MRGLocaleDemo/MRGLocaleDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MRGLocaleDemo
+//
+//  Created by Vincent Roy Chevalier on 2014-03-06.
+//  Copyright (c) 2014 Mirego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MRGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRGAppDelegate class]));
+    }
+}
